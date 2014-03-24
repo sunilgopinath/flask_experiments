65c863b9e04203c7059d9030ed2f9857eb9d8de5
sunilgopinath <sxg282@gmail.com>
2014-03-24 15:57:41 -0400

    refactored into packages

--- api.py
+++ api.py
@@ -1,14 +1,4 @@
-from flask import Flask
-from flask.ext import restful
-
-app = Flask(__name__)
-api = restful.Api(app)
-
-class HelloWorld(restful.Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+from flask_experiments import app
 
 if __name__ == '__main__':
     app.run(debug=True)
